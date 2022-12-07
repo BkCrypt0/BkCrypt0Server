2f194ecd5ff19b3037009d84a3a4f5ae482cdb7b
rikikudohust <trunghieubh0102@gmail.com>
2022-12-07 15:25:29 +0700

    Update Index.js

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 import cors from "cors";
-import { claimRouter } from "./src/routes/claimRouter";
+// import { claimRouter } from "./src/routes/claimRouter";
 import { userRouter } from "./src/routes/userRouter";
 import { publishedRouter } from "./src/routes/publishedRouter";
 import { issueRouter } from "./src/routes/issueRouter";
 import { mappingRouter } from "./src/routes/mappingRouter";
-import { revokeRouter } from "./src/routes/revokeRouter";
-import {testRouter} from "./src/routes/testRouter";
+// import { revokeRouter } from "./src/routes/revokeRouter";
 
 //Dotenv
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
@@ -17,7 +16,7 @@ import {CacheDB, ClaimTreeCache, connectMongo, Eddsa, LevelDB, RevokeCache, Revo
 import { authenRouter } from "./src/routes/authorizationRouter";
 import { hashRouter } from "./src/routes/hashRouter";
 import { hashMimc } from "./connect";
-import { unrevokeRouter } from "./src/routes/unrevokeRouter";
+// import { unrevokeRouter } from "./src/routes/unrevokeRouter";
 
 var app = express();
 const port = process.env.PORT; // default port to listen
@@ -25,16 +24,16 @@ connectMongo()
 
 app.use(json());
 app.use(cors());
-app.use("/claimed", claimRouter);
+// app.use("/claimed", claimRouter);
 app.use("/users", userRouter);
 app.use("/published", publishedRouter);
 app.use("/issue", issueRouter);
 app.use("/mapping", mappingRouter);
-app.use("/revoke", revokeRouter)
-app.use("/test", testRouter);
+// app.use("/revoke", revokeRouter)
+// app.use("/test", testRouter);
 app.use("/authen", authenRouter);
 app.use("/hash", hashRouter)
-app.use("/unrevoke", unrevokeRouter);
+// app.use("/unrevoke", unrevokeRouter);
 
 // define a route handler for the default home page
 app.get("/", async (req, res) => {
